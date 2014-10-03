7fe19d4ff765e07e3300fe83388dff9f31f283cc
Vinicius Souza <hi@vsouza.com>
2014-10-03 16:34:25 -0300

    add gcm

--- gcm.py
+++ gcm.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 import time
+from gcm import GCM
+import argparse
+
+# API Key for your Google OAuth project
+API_KEY = ''
 
 class Gcm(object):
     """Send push notifications for Android devices"""
@@ -10,6 +15,11 @@ class Gcm(object):
     def __init__(self):
         super(Apns, self).__init__()
 
+    def send_push_notification(registration_id, message):
+        gcm = GCM(API_KEY)
+        resp = gcm.plaintext_request(registration_id=registration_id,
+                                 data={'message': message})
+
 
     def send(self):
         print self.number
@@ -18,3 +28,8 @@ if __name__ == '__main__':
     x = Apns()
     x.number = 99999999
     x.send()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--reg-id', dest='registration_id', required=True)
+    parser.add_argument('-m', '--message', dest='message', required=True)
+    args = parser.parse_args()
+    x.send_push_notification(args.registration_id, args.message)
