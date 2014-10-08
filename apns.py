ecb2b6632bb3a3759f881b6273eba3454f167302
Vinicius Souza <hi@vsouza.com>
2014-10-07 22:34:28 -0300

    Apple Push Notifications 0.1

--- apns.py
+++ apns.py
@@ -5,16 +5,18 @@ import json
 import struct
 import socket
 import ssl
+import binascii
+import config
+
 class Apns(object):
     """Send push notifications to iOS devices."""
 
-    number = 0
-    deviceToken = None
-    server = "ssl://gateway.sandbox.push.apple.com"
+    deviceToken = config.device_token
+    pushServer = "gateway.sandbox.push.apple.com"
     port = 2195
     timeout = 6000
-    keyfile = "/certificates/ios_development.cert"
-    certfile = "/certificates/ios_development.cert"
+    keyfile = config.certificates_dir + "/key.pem"
+    certfile = config.certificates_dir + "/pushcert.pem"
 
     def __init__(self):
         super(Apns, self).__init__()
@@ -25,20 +27,25 @@ class Apns(object):
         body = {}
         body['aps'] = {'badge':badge, 'alert': message, 'sound': sound}
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ssl_sock = ssl.wrap_socket(s, self.keyfile, self.certfile, do_handshake_on_connect=False)
+        s = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM)
 
-        socket.timeout(0)
+        s.connect((self.pushServer, self.port))
+        ssl_sock = ssl.wrap_socket(s, self.keyfile, self.certfile)
         ssl_sock.setblocking(False)
-        ssl_sock.connect((self.server, self.port))
 
         payload = json.dumps(body)
-        msg = chr(0) . struct.pack('n', 32) . struct.pack('H*', deviceToken) . struct.pack('n', strlen(payload)) . payload;
-
-        pprint.pprint(ssl_sock.getpeercert())
+        
+        token = binascii.unhexlify(self.deviceToken)
+        fmt = '!cH32sH{0:d}s'.format(len(payload))
+        cmd = '\x00'
+        message = struct.pack(fmt, cmd, len(token), token, len(payload), payload)
+        ssl_sock.write(message)
+        
         ssl_sock.close()
+
+        return True
 
 if __name__ == '__main__':
     x = Apns()
-    x.number = 99999999
     x.send(1, "teste", "default")
